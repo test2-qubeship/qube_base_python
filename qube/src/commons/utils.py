3f3252de96548cafa44a5695e4aa1c8268154206
Omar Khalid <omar@qubeship.io>
2017-02-14 02:10:58 -0800

    Optimized the normalization. Pep8 the code.

--- qube/src/commons/utils.py
+++ qube/src/commons/utils.py
@@ -1,5 +1,4 @@
 def clean_nonserializable_attributes(dictionary):
-  #normalize the name for 'id'
-  if '_id' in dictionary:
-    dictionary['id'] = str(dictionary['_id'])
-    del dictionary['_id']
+    # normalize the name for 'id'
+    if '_id' in dictionary:
+        dictionary['id'] = str(dictionary.pop('_id'))
