ebbe27dce9fe637b5641e2db0a39bc22fa3b5bf6
Omar Khalid <omar@qubeship.io>
2017-02-15 05:45:28 -0800

    bats changes

--- bats/run_bats.sh
+++ bats/run_bats.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $DIR
+cd $DIR/..
 set -o allexport
 #load .env.sh. switch dir to make 'cat ./qube.yaml' work
-pushd ..
-source .env.sh
-popd
+#pushd ..
+source .env
+#popd
 
 echo "checkout /tmp/test.out for info"
 echo "checkout /tmp/error.out for errors"
@@ -12,4 +14,4 @@ echo "checkout /tmp/error.out for errors"
 rm /tmp/test.out 2> /dev/null
 rm /tmp/error.out 2> /dev/null
 
-bats tests.bats
+bats bats/tests.bats
