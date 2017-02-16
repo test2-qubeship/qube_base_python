26c30623f79bef42230cfb824a0bb75f249ca6cf
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2017-02-15 18:03:37 -0800

    swagger enhancements

--- qube/src/api/app.py
+++ qube/src/api/app.py
@@ -6,7 +6,10 @@ from logging.config import fileConfig
 import os
 
 from flask import Flask
+
 from flask_restful_swagger_2 import Api
+from flask_swagger_ui import get_swaggerui_blueprint
+
 from pkg_resources import resource_filename
 from qube.src.api.helloworld import HelloWorld
 from qube.src.commons.log import Log as LOG
@@ -17,13 +20,30 @@ logging_config = resource_filename(
 fileConfig(logging_config)
 
 app = Flask(__name__)
-api = Api(app, api_version='0.1', api_spec_url='/specs')
+API_URL = '/specs' # Our API url (can of course be a local resource)
+
+api = Api(app, api_version='0.1', api_spec_url=API_URL)
 DEFAULT_HOST = os.environ['DEFAULT_LISTENER_HOST']
 DEFAULT_PORT = int(os.environ.get('DEFAULT_LISTENER_PORT', '5000'))
 DEBUG = os.environ.get('DEBUG', 'False') \
     in ("yes", "y", "true", "True", "t", "1")
 
-api.add_resource(HelloWorld, '/<name>')
+api.add_resource(HelloWorld, '/hello')
+
+SWAGGER_URL = '/api/docs' # URL for exposing Swagger UI (without trailing '/')
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+  SWAGGER_URL, # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+  API_URL,
+  #config={ # Swagger UI config overrides
+  #'supportedSubmitMethods': ['get']
+  #}
+)
+
+# Register blueprint at URL
+# (URL must match the one given to factory function above)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 
 def main():
