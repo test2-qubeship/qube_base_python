76332b8dd201cb72214c779cb2ee7dd28c0d8acd
nidve01 <veerendrakumar.niddapu@ca.com>
2017-02-16 17:30:44 -0800

    unit test of create model with mock

--- qube/src/api/__init__.py
+++ qube/src/api/__init__.py
@@ -12,9 +12,9 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-app.config['MONGOALCHEMY_CONNECTION_STRING'] = os.environ['MONGOALCHEMY_CONNECTION_STRING']
-app.config['MONGOALCHEMY_SERVER'] = os.environ['MONGOALCHEMY_SERVER']
-app.config['MONGOALCHEMY_PORT'] = os.environ['MONGOALCHEMY_PORT']
-app.config['MONGOALCHEMY_DATABASE'] = os.environ['MONGOALCHEMY_DATABASE']
+app.config['MONGOALCHEMY_CONNECTION_STRING'] = os.getenv('MONGOALCHEMY_CONNECTION_STRING','')
+app.config['MONGOALCHEMY_SERVER'] = os.getenv('MONGOALCHEMY_SERVER','')
+app.config['MONGOALCHEMY_PORT'] = os.getenv('MONGOALCHEMY_PORT',0)
+app.config['MONGOALCHEMY_DATABASE'] = os.getenv('MONGOALCHEMY_DATABASE','')
 
 persist_db = MongoAlchemy(app)
---
+++ qube/test/test_hello_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+"""
+Add docstring here
+"""
+import os
+import unittest
+import mock
+import mongomock
+from mock import patch
+
+#from qube.src.api import app
+
+#app.config = MagicMock(return_value=None)
+class TestHelloModel(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print("before class")
+        
+    @mock.patch('pymongo.mongo_client.MongoClient', new=mongomock.MongoClient)
+    def test_create_hello_model(self):
+        from qube.src.models.hello import Hello
+        hello_data = Hello(name='testname')
+        with patch('mongomock.write_concern.WriteConcern.__init__',return_value=None):
+            hello_data.save()
+        
+        self.assertTrue(hello_data.mongo_id is not None)
+        print hello_data.mongo_id
+
+    @classmethod
+    def tearDownClass(cls):
+        print("After class")
+
+if __name__ == '__main__':
+    unittest.main()
