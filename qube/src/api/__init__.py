dacdcd718f539c1cbe3c0718d981589e8dc0f332
Zhang, Jiang <jiang@qubeship.io>
2017-02-23 13:29:03 -0800

    environment variable adjustment

--- qube/src/api/__init__.py
+++ qube/src/api/__init__.py
@@ -13,8 +13,11 @@ app = Flask('Hello')
 CORS(app)
 app.config['MONGOALCHEMY_CONNECTION_STRING'] = \
     os.getenv('QUBE_SERVICE_MONGOALCHEMY_CONNECTION_STRING', '')
-app.config['MONGOALCHEMY_SERVER'] = os.getenv('QUBE_SERVICE_MONGOALCHEMY_SERVER', '')
-app.config['MONGOALCHEMY_PORT'] = os.getenv('QUBE_SERVICE_MONGOALCHEMY_PORT', 0)
-app.config['MONGOALCHEMY_DATABASE'] = os.getenv('QUBE_SERVICE_MONGOALCHEMY_DATABASE', '')
+app.config['MONGOALCHEMY_SERVER'] = \
+    os.getenv('QUBE_SERVICE_MONGOALCHEMY_SERVER', '')
+app.config['MONGOALCHEMY_PORT'] = \
+    os.getenv('QUBE_SERVICE_MONGOALCHEMY_PORT', 0)
+app.config['MONGOALCHEMY_DATABASE'] = \
+    os.getenv('QUBE_SERVICE_MONGOALCHEMY_DATABASE', '')
 
 persist_db = MongoAlchemy(app)
