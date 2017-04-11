d0a1855807fbc3ee8e47b1871be8a273a8564fa5
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2017-04-11 01:11:52 -0700

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -4,5 +4,5 @@ cd $DIR/
 
 set -o allexport
 source env-init.sh
-
+mongod --storageEngine inMemory --fork --logpath /var/log/mongod.log
 uwsgi --http "${DEFAULT_LISTENER_HOST}":"${DEFAULT_LISTENER_PORT}" --http-keepalive --add-header "Connection: keep-alive" --pyargv "--debug" --module qube.src.api.app --callable app --no-site --pythonpath=/usr/local/lib/python3.5 --pythonpath=/usr/local/lib/python3.5/site-packages --processes "${DEFAULT_PROCESS_COUNT}" --enable-threads 2>&1 > nohup.out
